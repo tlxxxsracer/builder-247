b45ce0b564b822b38940a84528a0148213468909
tlxxxsracer <tlxxxsracer@users.noreply.github.com>
2025-05-06 13:47:59 +0000

    Update routes to use signature validation middleware

--- middle-server/src/routes/routes.ts
+++ middle-server/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router, RequestHandler } from "express";
 import { verifyBearerToken } from "../middleware/auth";
+import { validateSignature } from "../middleware/signature_validation";
 
 /******** Builder *********/
 import { fetchTodo } from "../controllers/builder/fetchToDo";
@@ -38,41 +39,38 @@ import { info } from "../controllers/prometheus/info";
 const router = Router();
 
 /********** Builder ***********/
-router.post("/builder/fetch-to-do", fetchTodo as RequestHandler);
-router.post("/builder/add-aggregator-info", addAggregatorInfo as RequestHandler);
-router.post("/builder/add-pr-to-to-do", addPR as RequestHandler);
-router.post("/builder/add-issue-pr", addIssuePR as RequestHandler);
-router.post("/builder/check-to-do", checkToDo as RequestHandler);
-router.post("/builder/assign-issue", assignIssue as RequestHandler);
-router.post("/builder/update-audit-result", updateAuditResult as RequestHandler);
-router.post("/builder/fetch-issue", fetchIssue as RequestHandler);
-router.post("/builder/check-issue", checkIssue as RequestHandler);
-router.get("/builder/get-source-repo/:nodeType/:uuid", getSourceRepo as RequestHandler);
+router.post("/builder/fetch-to-do", validateSignature, fetchTodo as RequestHandler);
+router.post("/builder/add-aggregator-info", validateSignature, addAggregatorInfo as RequestHandler);
+router.post("/builder/add-pr-to-to-do", validateSignature, addPR as RequestHandler);
+router.post("/builder/add-issue-pr", validateSignature, addIssuePR as RequestHandler);
+router.post("/builder/check-to-do", validateSignature, checkToDo as RequestHandler);
+router.post("/builder/assign-issue", validateSignature, assignIssue as RequestHandler);
+router.post("/builder/update-audit-result", validateSignature, updateAuditResult as RequestHandler);
+router.post("/builder/fetch-issue", validateSignature, fetchIssue as RequestHandler);
+router.post("/builder/check-issue", validateSignature, checkIssue as RequestHandler);
+router.get("/builder/get-source-repo/:nodeType/:uuid", validateSignature, getSourceRepo as RequestHandler);
 
 /********** Summarizer */
-router.post("/summarizer/fetch-summarizer-todo", fetchSummarizerRequest as RequestHandler);
-router.post("/summarizer/add-pr-to-summarizer-todo", addSummarizerRequest as RequestHandler);
-router.post("/summarizer/trigger-fetch-audit-result", triggerFetchAuditResultSummarizer as RequestHandler);
-router.post("/summarizer/check-summarizer", checkSummarizerRequest as RequestHandler);
-// router.post("/summarizer/trigger-update-swarms-status", triggerUpdateSwarmsStatus as RequestHandler);
-// router.post("/summarizer/trigger-save-swarms-for-round", triggerSaveSwarmsForRound as RequestHandler);
+router.post("/summarizer/fetch-summarizer-todo", validateSignature, fetchSummarizerRequest as RequestHandler);
+router.post("/summarizer/add-pr-to-summarizer-todo", validateSignature, addSummarizerRequest as RequestHandler);
+router.post("/summarizer/trigger-fetch-audit-result", validateSignature, triggerFetchAuditResultSummarizer as RequestHandler);
+router.post("/summarizer/check-summarizer", validateSignature, checkSummarizerRequest as RequestHandler);
 
 /********** Planner ***********/
-router.post("/planner/fetch-planner-todo", fetchPlannerRequest as RequestHandler);
-router.post("/planner/add-pr-to-planner-todo", addPlannerRequest as RequestHandler);
-router.post("/planner/check-planner", checkPlannerRequest as RequestHandler);
-router.post("/planner/trigger-fetch-audit-result", triggerFetchAuditResultPlanner as RequestHandler);
+router.post("/planner/fetch-planner-todo", validateSignature, fetchPlannerRequest as RequestHandler);
+router.post("/planner/add-pr-to-planner-todo", validateSignature, addPlannerRequest as RequestHandler);
+router.post("/planner/check-planner", validateSignature, checkPlannerRequest as RequestHandler);
+router.post("/planner/trigger-fetch-audit-result", validateSignature, triggerFetchAuditResultPlanner as RequestHandler);
 
 /*********** Prometheus Website ***********/
-router.get("/prometheus/get-assigned-nodes", getAssignedTo as RequestHandler);
-router.post("/prometheus/classification", verifyBearerToken, classification as RequestHandler);
-router.get("/prometheus/info", info as RequestHandler);
-
+router.get("/prometheus/get-assigned-nodes", validateSignature, getAssignedTo as RequestHandler);
+router.post("/prometheus/classification", verifyBearerToken, validateSignature, classification as RequestHandler);
+router.get("/prometheus/info", validateSignature, info as RequestHandler);
 
 /****************** Supporter **************/
-router.post("/supporter/bind-key-to-github", bindRequest as RequestHandler);
-router.post("/supporter/fetch-repo-list", fetchRepoList as RequestHandler);
-router.post("/supporter/check-request", checkRepoRequest as RequestHandler);
+router.post("/supporter/bind-key-to-github", validateSignature, bindRequest as RequestHandler);
+router.post("/supporter/fetch-repo-list", validateSignature, fetchRepoList as RequestHandler);
+router.post("/supporter/check-request", validateSignature, checkRepoRequest as RequestHandler);
 
 router.get("/hello", (req, res) => {
   res.json({ message: "Hello World!" });
